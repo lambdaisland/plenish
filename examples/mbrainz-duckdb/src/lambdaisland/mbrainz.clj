0f36b719570b7382f505c0f168d375d67f7be10c
Laurence Chen <laurence@replware.dev>
2023-05-04 21:01:15 +0800

    add duckdb deps.edn

---
+++ examples/mbrainz-duckdb/src/lambdaisland/mbrainz.clj
@@ -0,0 +1,19 @@
+(ns lambdaisland.mbrainz
+  (:require
+   [datomic.api :as d]
+   [lambdaisland.plenish :as plenish]
+   [next.jdbc :as jdbc]
+   [next.jdbc.result-set :as rs]))
+
+(def datomic-conn (d/connect "datomic:dev://localhost:4334/mbrainz-1968-1973"))
+(def pg-conn (jdbc/get-datasource "jdbc:pgsql://localhost:5432/mbrainz?user=plenish&password=plenish"))
+
+(def metaschema
+  {:tables {:release/name {}
+            :artist/name {}}})
+
+(def initial-ctx (plenish/initial-ctx datomic-conn metaschema))
+
+(def new-ctx (plenish/import-tx-range
+              initial-ctx datomic-conn pg-conn
+              (d/tx-range (d/log datomic-conn) nil nil)))
